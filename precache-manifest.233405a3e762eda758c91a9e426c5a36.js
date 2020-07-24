723f62bfe933ee7f3294a9aad24b43f68c1cd067
Jenya <jeenjenya@gmail.com>
2020-07-24 17:08:44 +0800

    Updates

---
+++ precache-manifest.233405a3e762eda758c91a9e426c5a36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d67ec88f52a8975b45021a4155956b17",
+    "url": "/test-newton/index.html"
+  },
+  {
+    "revision": "07a42d8c82c44e5a3ad3",
+    "url": "/test-newton/static/css/main.d1724c16.chunk.css"
+  },
+  {
+    "revision": "dad939af1d9898f1a1d7",
+    "url": "/test-newton/static/js/2.450fe9e1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/test-newton/static/js/2.450fe9e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07a42d8c82c44e5a3ad3",
+    "url": "/test-newton/static/js/main.25eb7405.chunk.js"
+  },
+  {
+    "revision": "e758ef6c91c32ea6a8d6",
+    "url": "/test-newton/static/js/runtime-main.48674b11.js"
+  }
+]);
